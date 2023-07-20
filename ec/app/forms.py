4a860b5468c3a83973ae117bc6c9defbb3cae8c2
nisha <nishachauhan0318@gmail.com>
2023-07-20 12:37:43 +0530

    done forgot password

--- ec/app/forms.py
+++ ec/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm,SetPasswordForm,PasswordResetForm
 from django.contrib.auth.models import User
 from .models import Customer 
 
@@ -25,11 +25,11 @@ class MyPasswordChangeForm(PasswordChangeForm):
     new_password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
-class MyPasswordResetForm(PasswordChangeForm):
+class MyPasswordResetForm(PasswordResetForm):
     email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 
 
-class SetPasswordForm(PasswordChangeForm):
+class MySetPasswordForm(SetPasswordForm):
     new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     new_password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
       
--- ec/app/urls.py
+++ ec/app/urls.py
@@ -5,7 +5,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm,MyPasswordResetForm
+from .forms import LoginForm,MyPasswordResetForm,MySetPasswordForm
 urlpatterns = [
     path('',views.home), 
     path('home/',views.home ,name='home'),
@@ -20,19 +20,23 @@ urlpatterns = [
     path('updateAddress/<int:pk>',views.updateAddress.as_view(),name='updateAddress'),
     
     
-    #login Authentication
+    #login Authentication\
+    #registration path
     path('registration/',views.CustomerRegistratuonView.as_view(),name="customerregistration"),
     
     path('accounts/login/',auth_view.LoginView.as_view(template_name='app/login.html' ,authentication_form=LoginForm),name="login"),
-    
-   path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'), name='passwordchange'),
-     
+    #password change paths!
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'), name='passwordchange'),
     path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
-    
+    #logout
     path('logout/',auth_view.LogoutView.as_view(next_page='login'),name='logout'),
     
+    #passworrd reset/Forgot paths!
     path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html' ,form_class=MyPasswordResetForm),name="password_reset"),
-    path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html' ,form_class=MyPasswordResetForm),name="password_reset"),
+    path('password-reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html' ,form_class=MySetPasswordForm),name="password_reset_confirm"),
+    path('password-reset-complete/',auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'),name="password_reset_complete"),
+    
     
     
   
